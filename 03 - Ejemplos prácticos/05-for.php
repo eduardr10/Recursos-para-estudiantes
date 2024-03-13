5621665552358231fcbfcfcb3d6c8b3a1d94f6ba
Eduard Espinoza <eduardr1091@gmail.com>
2024-03-13 13:04:19 -0600

    For. Bucle

---
+++ 03 - Ejemplos prácticos/05-for.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Usando el mismo ejemplo del if, podríamos aprovecharnos para ver cómo funciona el bucle for.
+ * 
+ * Glosario:
+ * * Bucle: Estructura quue permite realizar un fragmento de código repetidas veces.
+ * * Iteración: Es el nombre que le damos a cada vez que el ciclo se ejecuta.
+ * * Variable de control: Se usa para llevar la cuenta del número de iteraciones.
+ * * Condición de inicio: Condición que se evalúa al principio del bucle para determinar si se ejecuta el bloque de código.
+ * * Condición de parada: Es la condición que se debe cumplir para detener la ejecución del bucle.
+ * * Incremento: La operación que se realiza al final de cada iteración
+ * 
+ * La sintaxis del for es:
+ * for($i = 0; $i < 5; $i++){
+ *  // Código que se repetirá
+ * }
+ */
